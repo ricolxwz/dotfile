c59bcd73d869c068ff3089cce27883652f10890a
wenzexu <ricol.xwz@outlook.com>
2024-10-14 10:17:52 +1100

    update nvim

---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,16 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  event = "VeryLazy",
+  config = function(_, opts)
+    -- Custom theme configuration for lualine
+    local auto = require("lualine.themes.auto")
+    local lualine_modes = { "insert", "normal", "visual", "command", "replace", "inactive", "terminal" }
+    for _, field in ipairs(lualine_modes) do
+      if auto[field] and auto[field].c then
+        auto[field].c.bg = "NONE" -- Remove background color for certain modes
+      end
+    end
+    opts.options.theme = auto -- Apply the customized theme to lualine
+    require("lualine").setup(opts) -- Setup lualine with modified options
+  end,
+}
