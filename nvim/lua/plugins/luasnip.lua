2870e9ad6e3ba111be0d537566b7cd09e44e3cc5
wenzexu <ricol.xwz@outlook.com>
2024-10-15 10:22:43 +1100

    update nvim

---
+++ nvim/lua/plugins/luasnip.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "L3MON4D3/LuaSnip",
+    build = (not LazyVim.is_win())
+        and "echo 'NOTE: jsregexp is optional, so not a big deal if it fails to build'; make install_jsregexp"
+      or nil,
+    dependencies = {
+      {
+        "rafamadriz/friendly-snippets",
+      },
+    },
+    opts = function(_, opts)
+      opts.history = true
+      opts.delete_check_events = "TextChanged"
+      require("luasnip.loaders.from_vscode").lazy_load()
+      require("luasnip.loaders.from_vscode").lazy_load({
+        paths = { "~/dotfile/snippets/" },
+      })
+    end,
+  },
+}
--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -1,36 +1,34 @@
 return {
   {
-    "L3MON4D3/LuaSnip",
-    keys = function()
-      return {}
-    end,
-  },
-  {
     "hrsh7th/nvim-cmp",
     dependencies = {
       "hrsh7th/cmp-emoji",
+      "saadparwaiz1/cmp_luasnip",
     },
-    ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
       local cmp = require("cmp")
-      cmp.setup({
-        enabled = function()
-          local filetype = vim.bo.filetype
-          if filetype == "markdown" then
-            return false
-          else
-            return true
-          end
+      opts.snippet = {
+        expand = function(args)
+          require("luasnip").lsp_expand(args.body)
         end,
-        window = {
-          documentation = cmp.config.window.bordered({
-            winhighlight = "Normal:CmpPmenu,FloatBorder:CmpPmenuBorder,CursorLine:None,Search:None",
-          }),
-          completion = cmp.config.window.bordered({
-            winhighlight = "Normal:CmpPmenu,FloatBorder:CmpPmenuBorder,CursorLine:None,Search:None",
-          }),
-        },
-      })
+      }
+      table.insert(opts.sources, { name = "luasnip" })
+      opts.window = {
+        documentation = cmp.config.window.bordered({
+          winhighlight = "Normal:CmpPmenu,FloatBorder:CmpPmenuBorder,CursorLine:None,Search:None",
+        }),
+        completion = cmp.config.window.bordered({
+          winhighlight = "Normal:CmpPmenu,FloatBorder:CmpPmenuBorder,CursorLine:None,Search:None",
+        }),
+      }
+      opts.enabled = function()
+        local filetype = vim.bo.filetype
+        if filetype == "python" then
+          return false
+        else
+          return true
+        end
+      end
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
         ["<Tab>"] = cmp.mapping(function(fallback)
           -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
