85d54c8852592887611716497ea0aa166d933b55
wenzexu <ricol.xwz@outlook.com>
2024-10-14 19:35:28 +1100

    update nvim

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,5 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 --
-vim.o.cursorline = false
-vim.o.pumblend = 0
+vim.opt.cursorline = false
+vim.opt.pumblend = 0
+vim.opt.spelllang = { "en", "cjk" }
