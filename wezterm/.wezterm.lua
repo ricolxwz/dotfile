d5fdfefcc1524b834dcc22449c08e7ca4c554fc1
wenzexu <ricol.xwz@outlook.com>
2024-10-15 15:41:18 +1100

    update wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -7,6 +7,6 @@ config.window_decorations = "RESIZE"
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
 config.font_size = 16.0
-config.window_background_opacity = 0.6
-config.macos_window_background_blur = 30
+config.window_background_opacity = 1.0
+-- config.macos_window_background_blur = 30
 return config
